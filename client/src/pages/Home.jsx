e16ead40ea2152ed9fb00cc857e3a13942758965
Pritiranjan Patra <patrapritiranjan11@gmail.com>
2025-04-18 09:54:11 +0530

    Final

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
           </div>
 
           {/* Center - Blog Feed */}
-          <div className="w-full lg:w-[60%] mt-16 flex justify-center items-center">
+          <div className="w-full lg:w-[50%] mt-16 flex justify-center items-center ">
             <div className="flex flex-col gap-6 w-full max-w-2xl">
               {blogs.length > 0 ? (
                 blogs.map((blog) => (
@@ -31,19 +31,31 @@ function Home() {
           </div>
 
           {/* Right - Advertisement */}
-          <div className="w-full lg:w-[20%] sticky top-20 h-[80vh] overflow-y-auto">
-            <div className="bg-gray-100 border border-gray-300 rounded-2xl p-4 text-center shadow-sm">
-              <h3 className="text-lg font-semibold mb-2">Advertisement</h3>
-              <p className="text-sm text-gray-500">Place your ad here!</p>
-              <div className="mt-4">
-                <img
-                  src="https://2.imimg.com/data2/IS/TN/MY-2561564/advertisement-designing-services-500x500.jpg"
-                  alt="Ad Banner"
-                  className="w-full rounded-lg"
-                />
-              </div>
-            </div>
-          </div>
+          <div className="w-full lg:w-[23%] sticky top-20 h-[80vh] overflow-y-auto">
+  <div className="grid grid-cols-2 gap-4 p-2">
+    <div className="col-span-1 bg-gray-100 border border-gray-300 rounded-2xl p-2 text-center shadow-sm">
+      <img
+        src="https://img.freepik.com/free-vector/beautiful-cosmetic-ad_23-2148471068.jpg?semt=ais_hybrid&w=740"
+        alt="Ad 1"
+        className="w-full rounded-lg"
+      />
+    </div>
+    <div className="col-span-1 bg-gray-100 border border-gray-300 rounded-2xl p-2 text-center shadow-sm">
+      <img
+        src="https://asset.gecdesigns.com/img/social-media-poster-templates/furniture-shop-advertisement-instagram-poster-template-1683124415404-cover.webp"
+        alt="Ad 2"
+        className="w-full rounded-lg"
+      />
+    </div>
+    <div className="col-span-2 bg-gray-100 border border-gray-300 rounded-2xl p-2 text-center shadow-sm">
+      <img
+        src="https://2.imimg.com/data2/IS/TN/MY-2561564/advertisement-designing-services-500x500.jpg"
+        alt="Ad 3"
+        className="w-full rounded-lg"
+      />
+    </div>
+  </div>
+</div>
         </div>
       </div>
 
